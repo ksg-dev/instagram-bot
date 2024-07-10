332380893b982d837e1f83ed5681124c39ed4a95
ksg-dev <ksg.dev.data@gmail.com>
2024-07-10 11:56:34 -0500

    login to instagram and close popups

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import time
+
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
@@ -11,6 +14,9 @@ INSTA_PW = os.environ["INSTA_PW"]
 INSTA_EMAIL = os.environ["INSTA_EMAIL"]
 SIMILAR_ACCT = os.environ["SIMILAR_ACCOUNT"]
 
+# INSTA_USER_XPATH =
+# INSTA_PASSWORD_XPATH
+
 
 class InstaFollower:
     def __init__(self):
@@ -19,7 +25,29 @@ class InstaFollower:
         self.driver = webdriver.Chrome(options=chrome_options)
 
     def login(self):
-        pass
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        time.sleep(2)
+        username = self.driver.find_element(By.NAME, value="username")
+        username.send_keys(INSTA_USER)
+        password = self.driver.find_element(By.NAME, value="password")
+        password.send_keys(INSTA_PW)
+        password.send_keys(Keys.ENTER)
+        time.sleep(7)
+
+        no_save_login = self.driver.find_element(By.CLASS_NAME, value="x1i10hfl")
+        no_save_login.click()
+        time.sleep(5)
+
+        try:
+            notification_button = self.driver.find_element(By.CSS_SELECTOR, value="._a9_1")
+
+        except NoSuchElementException:
+            pass
+
+        else:
+            notification_button.click()
+
+
 
     def find_followers(self):
         pass
@@ -30,6 +58,6 @@ class InstaFollower:
 
 bot = InstaFollower()
 bot.login()
-bot.find_followers()
-bot.follow()
+# bot.find_followers()
+# bot.follow()
 
